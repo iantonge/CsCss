2e819d3b807b863ba7059af142ed4ce33c9351ee
Ian Tonge <ian.tonge@gmail.com>
2022-02-07 19:16:09 +0000

    Add test project with initial tests

---
+++ CsCss.Tests/Values/Numeric/CalcTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+using static CsCss.Statics;
+
+namespace CsCss.Tests.Values.Numeric
+{
+    public class CalcTests
+    {
+        [Fact]
+        public void RendersLengthSubtraction()
+        {
+            var result = Calc(100.Percent() - 5.Px());
+            Assert.Equal("calc(100% - 5px)", result.ToString());
+        }
+
+        [Fact]
+        public void RendersLengthAddition()
+        {
+            var result = Calc(100.Percent() + 5.Px());
+            Assert.Equal("calc(100% + 5px)", result.ToString());
+        }
+
+        [Fact]
+        public void RendersLengthMultiplication()
+        {
+            var result = Calc(10.Percent() * 5);
+            Assert.Equal("calc(10% * 5)", result.ToString());
+        }
+
+        [Fact]
+        public void RendersLengthDivision()
+        {
+            var result = Calc(100.Percent() / 5);
+            Assert.Equal("calc(100% / 5)", result.ToString());
+        }
+
+        [Fact]
+        public void RendersComplexCalculation()
+        {
+            var result = Calc(100.Percent() - 5.Px() / 5);
+            Assert.Equal("calc(100% - 5px / 5)", result.ToString());
+        }
+
+        [Fact]
+        public void RendersComplexCalculationWithBrackets()
+        {
+            var result = Calc((100.Percent() - 5.Px()) / 5);
+            Assert.Equal("calc((100% - 5px) / 5)", result.ToString());
+        }
+    }
+}
