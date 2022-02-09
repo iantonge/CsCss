45d5bf1abc421d9abe73f63ed4f2215bda76cf35
Ian Tonge <ian.tonge@gmail.com>
2022-02-09 12:07:23 +0000

    Add string value tests

---
+++ CsCss.Tests/Values/Text/String/StringValueTests.cs
@@ -0,0 +1,22 @@
+﻿using CsCss.Values.Text.String;
+using Xunit;
+
+namespace CsCss.Tests.Values.Text.String
+{
+    public class StringValueTests
+    {
+        [Fact]
+        public void RendersStringValues()
+        {
+            var stringValue = new StringValue("test");
+            Assert.Equal("'test'", stringValue.ToString());
+        }
+
+        [Fact]
+        public void CanConvertFromString()
+        {
+            StringValue stringValue = "test";
+            Assert.Equal("'test'", stringValue.ToString());
+        }
+    }
+}
---
+++ CsCss/Values/Text/String/StringValue.cs
@@ -0,0 +1,12 @@
+﻿namespace CsCss.Values.Text.String
+{
+    public class StringValue : TextValue
+    {
+        public StringValue(string v) : base($"'{v}'") { }
+
+        public static implicit operator StringValue(string value)
+        {
+            return new StringValue(value);
+        }
+    }
+}
